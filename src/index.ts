f9a92f6020fde6af6f60bc07668747297abdb8e9
Yappaholic <sav.boyar@gmail.com>
2024-05-12 14:40:51 +0300

    Add adding folders

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
 import { ToDo, Folder, Urgency } from "./classes";
+import "./style.css";
+const main = document.querySelector(".main");
+const create = document.querySelector(".add-folder");
+const module = document.querySelector("dialog");
+const submit = document.querySelector("form>button");
+const form = document.querySelector("form");
+
+function createFolder(name: string) {
+  const div = document.createElement("div");
+  const title = document.createElement("p");
+  const toggle = document.createElement("button");
+  div.classList.toggle("folder");
+  title.textContent = name;
+  toggle.textContent = "Open folder ▼";
+  div.appendChild(title);
+  div.appendChild(toggle);
+  main.appendChild(div);
+}
+create.addEventListener("click", () => {
+  module.showModal();
+});
+submit.addEventListener("click", (e) => {
+  e.preventDefault();
+  const data = new FormData(form);
+  const name = data.get("folder");
+  createFolder(name.toString());
+  module.close();
+});
